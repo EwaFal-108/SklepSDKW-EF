b511856f10f7eb148b294d86a7bfaea164472c60
Ewa <efaltynska@gmail.com>
2026-03-24 21:27:36 +0100

    Utworzenie modeli bazy danych (Film i Category) z relacjami

---
+++ Modele/Film.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SklepSDKW_EF.Modele
+{
+    public class Film
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nie podano tytułu")]
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        [StringLength(500)]
+        public string Desc { get; set; }
+
+        public decimal? Price { get; set; }
+
+        // Klucz obcy łączący Film z Kategorią
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        // Właściwość nawigacyjna
+        public Category Category { get; set; }
+    }
+}
---
+++ Modele/Kategoria.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SklepSDKW_EF.Modele
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Desc { get; set; }
+
+        // To połączenie z filmami (jeszcze będzie świecić na czerwono, póki nie dodamy klasy Film)
+        public ICollection<Film> Films { get; set; }
+    }
+}
