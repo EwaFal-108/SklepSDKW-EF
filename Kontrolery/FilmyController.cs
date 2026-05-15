492cf972d02f8d9ec0320cc061d6ec32f0c52a43
Ewa <efaltynska@gmail.com>
2026-05-15 16:57:07 +0200

    Aktualizacja menu nawigacyjnego - dodanie linku do filmów

---
+++ Kontrolery/FilmyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SklepSDKW_EF.DAL;
+using SklepSDKW_EF.Modele;
+
+namespace SklepSDKW_EF.Kontrolery
+{
+    public class FilmyController : Controller
+    {
+        private readonly SklepContext _db;
+
+        public FilmyController(SklepContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // Pobieramy filmy wraz z ich kategoriami z bazy danych
+            var filmy = _db.Filmy.Include(f => f.Category).ToList();
+            return View(filmy);
+        }
+    }
+}
