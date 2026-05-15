2f7a206af693f75725835810c0faa61ee505aab0
Ewa <efaltynska@gmail.com>
2026-05-15 18:10:41 +0200

    Dodanie akcji Details i widoku szczegółów filmu

--- Kontrolery/FilmyController.cs
+++ Kontrolery/FilmyController.cs
@@ -14,11 +14,34 @@ namespace SklepSDKW_EF.Kontrolery
             _db = db;
         }
 
+        // Metoda wyświetlająca listę wszystkich filmów
         public IActionResult Index()
         {
             // Pobieramy filmy wraz z ich kategoriami z bazy danych
             var filmy = _db.Filmy.Include(f => f.Category).ToList();
             return View(filmy);
+        }
+
+        // NOWA METODA: Szczegóły konkretnego filmu
+        // Wywoływana np. przez /Filmy/Details/1
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // Szukamy filmu o danym ID i dołączamy dane o kategorii
+            var film = _db.Filmy
+                .Include(f => f.Category)
+                .FirstOrDefault(m => m.Id == id);
+
+            if (film == null)
+            {
+                return NotFound();
+            }
+
+            return View(film);
         }
     }
 }
