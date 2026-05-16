768aac66917b0cf28bc315a8d1df6e973c17009b
Ewa <efaltynska@gmail.com>
2026-05-16 10:17:11 +0200

    Mechanizm sesji i infrastruktura dla mechanizmu koszyka; dodawanie produktów do koszyka

---
+++ Infrastructure/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using SklepSDKW_EF.Modele;
+
+namespace SklepSDKW_EF.Infrastructure
+{
+    public class CartItem
+    {
+        public Film Film { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal? Value { get; set; }
+
+    }
+}
+
---
+++ Infrastructure/Consts.cs
@@ -0,0 +1,8 @@
+﻿namespace SklepSDKW_EF.Infrastructure
+{
+    public class Consts
+    {
+        // Klucz sesji, pod którym zapisany będzie koszyk w pamięci przeglądarki
+        public static string CartSessionKey = "KoszykKlucz123";
+    }
+}
